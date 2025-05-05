21ad027d7558819130a184c01882d9382714e809
itncoffice <mufaddal2003@gmail.com>
2025-05-05 14:24:51 +0530

    new

--- src/main/java/com/mufaddal/employee/controller/EmployeeController.java
+++ src/main/java/com/mufaddal/employee/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://mufaddalcodes.in", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+@CrossOrigin("*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
